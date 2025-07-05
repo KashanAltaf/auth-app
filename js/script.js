f99f1a260c0208a1fd4209bfb3f21d4bcd10d7eb
KashanAltaf <kashanaltaf137@gmail.com>
2025-07-05 14:56:15 +0500

    Design Commit

--- js/script.js
+++ js/script.js
@@ -8,6 +8,16 @@ document.getElementById('signIn').addEventListener('click', () => {
     container.classList.remove('right-panel-active');
 });
 
+function togglePassword(eyeIcon) {
+    const input = eyeIcon.previousElementSibling;
+    if(input.type === "password"){
+        eyeIcon.classList.toggle('fa-eye-slash');
+    }
+    else{
+        eyeIcon.classList.toggle('fa-eye');
+    }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     const params = new URLSearchParams(window.location.search);
     if(params.get('error')){
